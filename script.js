211be5d8e7accfce8e3f49a9e2916de9cadad302
daehyun4998 <j6844998@naver.com>
2026-07-28 11:57:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
+
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', () => menu.classList.remove('open'));
+});
